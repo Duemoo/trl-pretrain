e0e8e76793e57c1f1e8f616f18745becb10c416f
Bread0288 <binlepain178@gmail.com>
2024-03-18 17:51:37 +0900

    intermediate sync

--- ner/classify_pretraining_corpus.py
+++ ner/classify_pretraining_corpus.py
@@ -1,12 +1,12 @@
-
-
 from ner import ner_spacy
 import numpy as np
 
 from cached_path import cached_path
 from olmo.config import TrainConfig
 from olmo.data import build_memmap_dataset
-
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import time
 
 def get_batch_instances(global_indices, dataset, batch_size: int, batch_idx: int) -> list[list[int]]:
     batch_start = batch_idx * batch_size
@@ -23,6 +23,10 @@ def main():
     data_order_file_path = cached_path("https://olmo-checkpoints.org/ai2-llm/olmo-small/46zc5fly/train_data/global_indices.npy")
     train_config_path = "../ner/OLMo_config/OLMo-1B.yaml"
     
+    # olmo = AutoModelForCausalLM.from_pretrained("allenai/OLMo-1B")
+    tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-1B",
+                                              trust_remote_code=True)
+    
     cfg = TrainConfig.load(train_config_path)
     dataset = build_memmap_dataset(cfg, cfg.data)
     # print(dataset[0])
@@ -30,10 +34,17 @@ def main():
     global_indices = np.memmap(data_order_file_path, mode="r+", dtype=np.uint32)
     
     # # Get all 2048 x 2048 token IDs in the first batch.
-    print(get_batch_instances(global_indices, dataset, batch_size, 0))
-
-    # print(ner_spacy("The Indian Space Research Organisation or is the national space agency of India, headquartered in Bengaluru. It operates under Department of Space which is directly overseen by the Prime Minister of India while Chairman of ISRO acts as executive of DOS as well."))
-
+    batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, 25500))
+    
+    start = time.time()
+    # <class: 'list'>, len : 2048
+    batch_in_text = tokenizer.batch_decode(batch)
+    end = time.time()
+    print(end-start)
+    start = time.time()
+    document_in_batch = "".join(batch_in_text)
+    # print(batch_in_text[0])
+    print(ner_spacy(document_in_batch))
 
 if __name__=="__main__":
     main()
--- ner/ner.py
+++ ner/ner.py
@@ -1,17 +1,21 @@
 import spacy
 
-def ner_spacy(text: str) -> list:
-    output = []
+def ner_spacy(text: str) -> dict:
+    output = {}
     # Faster and smaller pipeline, but less accurate
     # to use "en_core_web_sm", you should download the model first typing "python -m spacy download en_core_web_sm" in terminal
     # ner_model = spacy.load("en_core_web_sm", disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"])
     # Larger and slower pipeline, but more accurate
     # to use "en_core_web_trf", you should download the model first typing "python -m spacy download en_core_web_trf" in terminal
     ner_model = spacy.load("en_core_web_trf", disable=["tagger", "parser", "attribute_ruler", "lemmatizer"])
+    ner_model.max_length = 20000000
     ner_output = ner_model(text)
     # print(dir(text1))
 
     for word in ner_output.ents:
         # word.ents가 하나의 element만 포함하는지 확인할 필요 있음
-        output.append((word.ents[0], word.label_))
+        if (word.ents[0], word.label_) in output:
+            output[(word.ents[0], word.label_)] += 1
+        else:
+            output[(word.ents[0], word.label_)] = 1
     return output
