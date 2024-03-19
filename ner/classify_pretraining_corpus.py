89690fec01fa1d9638286e9c893f1135387aaa86
Bread0288 <binlepain178@gmail.com>
2024-03-19 06:04:46 +0000

    intermediate update

--- ner/classify_pretraining_corpus.py
+++ ner/classify_pretraining_corpus.py
@@ -6,9 +6,12 @@ from olmo.config import TrainConfig
 from olmo.data import build_memmap_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-import time, os, json
+import time, os, json, re
 import multiprocessing as mp
 from tqdm import tqdm
+from functools import partial
+import parmap
+
 
 def get_batch_instances(global_indices, dataset, batch_size: int, batch_idx: int) -> list[list[int]]:
     batch_start = batch_idx * batch_size
@@ -21,11 +24,29 @@ def get_batch_instances(global_indices, dataset, batch_size: int, batch_idx: int
     return batch_instances
 
 
-def count_entity(x):
+def count_entity_in_batch(x):
     document_index = x[0]
     text = x[1]
     ner_result = ner_spacy(str(text))
     return (document_index, ner_result)
+
+def check_entity_in_batch(step_idx, tokenizer, global_indices, dataset, batch_size):
+    print(f"step_idx: {step_idx}    PID : {os.getpid()}")
+    batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, step_idx))
+    batch_decoded = "".join(tokenizer.batch_decode(batch)).lower()
+    start = time.time()
+    if 'blizzard' in batch_decoded and 'hearthstone' in batch_decoded:
+        end = time.time()
+        print(end-start)
+        return (step_idx, True)
+    else:
+        end = time.time()
+        print(end-start)
+        return (step_idx, False)
+    
+def print_batch(x, tokenizer, global_indices, dataset, batch_size):
+    print(f"PID : {os.getpid()}     x: ({x})")
+    
     
 def main():
     data_order_file_path = cached_path("https://olmo-checkpoints.org/ai2-llm/olmo-small/46zc5fly/train_data/global_indices.npy")
@@ -44,26 +65,34 @@ def main():
     # # Get all 2048 x 2048 token IDs in the first batch.
     batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, 25500))
     
-    start = time.time()
     # <class: 'list'>, len : 2048
     batch_in_text = tokenizer.batch_decode(batch)
     document_in_batch = "".join(batch_in_text).split("<|endoftext|>")
-    end = time.time()
-    print(end-start)
+
+    print(document_in_batch[1])
     
-    cpu_num = 60
-    result_dict = {}
-    # for batch in batch_in_text[:4]:
-    #     print(ner_spacy(batch))
-    with mp.Pool(cpu_num) as pool:
-        result = list(tqdm(pool.imap(count_entity, enumerate(document_in_batch)), total=len(document_in_batch)))
-    for document_index, entities in result:
-        # 검출된 entity가 1개 이상일때만 해당 document 저장
-        if entities:
-            result_dict[int(document_index)] = entities
-        # print(list(pool.imap(count_entity, enumerate(document_in_batch[:4]))))
-    with open("./results/output_25500.json", "w") as f:
-        json.dump(result_dict, f)
+    # cpu_num = 60
+    # result_dict = {}
+    # with mp.Pool(cpu_num) as pool:
+        # result = list(tqdm(pool.imap(count_entity_in_batch, enumerate(document_in_batch)), total=len(document_in_batch)))
+    # for document_index, entities in result:
+    #     # 검출된 entity가 1개 이상일때만 해당 document 저장
+    #     if entities:
+    #         result_dict[int(document_index)] = entities
+    # with open("./results/output_25500.json", "w") as f:
+    #     json.dump(result_dict, f)
+    cpu_num = 2
+    finded_step = []
+    l = [(1,2), (3, 4), (5, 6), (7, 8)]
+    # with mp.Pool(cpu_num) as pool:
+    result = parmap.starmap(check_entity_in_batch, range(25501, 25501+100), tokenizer, global_indices, dataset, pm_pbar=True, pm_processes=mp.cpu_count())
+        # partial_func = partial(check_entity_in_batch, tokenizer, global_indices, dataset, batch_size)
+        # result = list(tqdm(pool.map(partial_func, range(25501, 25501+100)), total=100))
+    if result[1]:
+        finded_step.append(result[0])
+    print(finded_step)
+        
+        
 
 if __name__=="__main__":
     main()
