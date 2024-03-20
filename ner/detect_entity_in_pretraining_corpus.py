a988d9237781643f02cc518f9b79697bb25b655d
Bread0288 <binlepain178@gmail.com>
2024-03-20 21:54:34 +0900

    intermediate commit

--- ner/detect_entity_in_pretraining_corpus.py
+++ ner/detect_entity_in_pretraining_corpus.py
@@ -1,12 +1,16 @@
 from ner import ner_in_batch_spacy
+import numpy as np
+
 from cached_path import cached_path
 from olmo.config import TrainConfig
 from olmo.data import build_memmap_dataset
-from transformers import AutoTokenizer
-import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+import time, os, json, re
+import multiprocessing as mp
 from tqdm import tqdm
-
+from functools import partial
+import parmap
 
 data_order_file_path = cached_path("https://olmo-checkpoints.org/ai2-llm/olmo-small/46zc5fly/train_data/global_indices.npy")
 train_config_path = "../ner/OLMo_config/OLMo-1B.yaml"
@@ -27,31 +31,90 @@ def get_batch_instances(global_indices, dataset, batch_size: int, batch_idx: int
     return batch_instances
 
 
-if __name__=="__main__":
-    total_output = {}
-    start_index = 25501
-    end_index = 25501+5
+def count_entity_in_batch(x):
+    document_index = x[0]
+    text = x[1]
+    ner_result = ner_in_batch_spacy(str(text))
+    return (document_index, ner_result)
+
+# def check_entity_in_batch(step_idx, tokenizer, global_indices, dataset, batch_size):
+#     print(f"step_idx: {step_idx}    PID : {os.getpid()}")
+#     batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, step_idx))
+#     batch_decoded = "".join(tokenizer.batch_decode(batch)).lower()
+#     start = time.time()
+#     if 'blizzard' in batch_decoded and 'hearthstone' in batch_decoded:
+#         end = time.time()
+#         print(end-start)
+#         return (step_idx, True)
+#     else:
+#         end = time.time()
+#         print(end-start)
+#         return (step_idx, False)
+    
+def check_entity_in_batch(x, batch_size):
+    # print(f'Process {mp.current_process().name} started working on task {x}', flush=True)
+    batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, x))
     tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-1B", trust_remote_code=True)
-    for step_idx in tqdm(range(start_index, end_index)):
-        batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, batch_idx=step_idx))
-        # <class: 'list'>, len : 2048
-        batch_in_text = tokenizer.batch_decode(batch, skip_special_tokens=True)
-        # batch_in_text = "".join(batch_in_text).replace("<|endoftext|>", "  ")
-        ner_result = ner_in_batch_spacy(batch_in_text)
-        for entity in ner_result.keys():
-            print(entity)
-            if entity in total_output and ner_result[entity] == total_output[entity]["label"]:
-                total_output[entity]["step"].append(step_idx)
-            else:
-                total_output[entity] = {"label": ner_result[entity], "step" : [step_idx]}
+    batch_decoded = "".join(tokenizer.batch_decode(batch)).lower()
+    if 'blizzard' in batch_decoded and 'hearthstone' in batch_decoded:
+        result = (x, True)
+    else:
+        result = (x, False)
+    # print(f'Process {mp.current_process().name} ended working on task {x}', flush=True)
+    return result
     
-        # for line in batch_in_text:
-            
-        #     batch_in_text = "".join(line).replace("<|endoftext|>", "  ")
-        #     ner_result = ner_in_batch_spacy(line)
-        #     for entity in ner_result.keys():
-        #         if entity in total_output and ner_result[entity] == total_output[entity]["label"]:
-        #             total_output[entity]["step"].append(step_idx)
-        #         else:
-        #             total_output[entity] = {"label": ner_result[entity], "step" : [step_idx]}
     
+def main():
+    
+    # model load
+    # olmo = AutoModelForCausalLM.from_pretrained("allenai/OLMo-1B")
+    
+    # Part 1 : NER in specific batch and save it as json file
+    # Get all 2048 x 2048 token IDs in the specific batch.
+    batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, 25500))
+    tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-1B", trust_remote_code=True)
+    # <class: 'list'>, len : 2048
+    batch_in_text = tokenizer.batch_decode(batch)
+    document_in_batch = "".join(batch_in_text).split("<|endoftext|>")
+    
+    cpu_num = 60
+    result = ner_in_batch_spacy(document_in_batch, per_document=True)
+    print(result[0])
+    
+    with open("./results/output_25500.json", "w") as f:
+        json.dump(result, f)
+        
+        
+    # Part 2 : check a pair of entities in after batch
+    result = parmap.map(check_entity_in_batch, range(25501, 25501+100), batch_size, pm_pbar=True, pm_processes=100)
+    
+
+    
+    # with mp.Pool(cpu_num) as pool:
+    #     result = list(tqdm(pool.imap(count_entity_in_batch, enumerate(document_in_batch)), total=len(document_in_batch)))
+    # for document_index, entities in result:
+    #     # 검출된 entity가 1개 이상일때만 해당 document 저장
+    #     if entities:
+    #         result_dict[int(document_index)] = entities
+    
+
+    # start = time.time()
+    # result = [print_batch(i) for i in range(25501, 25501+100)]
+    # end = time.time()
+    # print(end-start)
+    # start = time.time()
+    # result = parmap.map(check_entity_in_batch, range(25501, 25501+100), batch_size, pm_pbar=True, pm_processes=100)
+    # end = time.time()
+    # print(end-start)
+    # print(result)
+    # print(len(result))
+        # partial_func = partial(check_entity_in_batch, tokenizer, global_indices, dataset, batch_size)
+        # result = list(tqdm(pool.map(partial_func, range(25501, 25501+100)), total=100))
+    # if result[1]:
+    #     finded_step.append(result[0])
+    # print(finded_step)
+        
+        
+
+if __name__=="__main__":
+    main()
--- ner/ner.py
+++ ner/ner.py
@@ -1,40 +1,136 @@
 import spacy
 import time
+import multiprocessing as mp
+import parmap
+from functools import partial
+import itertools
+from tqdm import tqdm
 
-def ner_in_batch_spacy(text: list) -> dict:
+
+class GenwithLen:
+    def __init__(self, generator, *args, **kwargs):
+        self.generator = generator
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.generator)
+
+    def __len__(self):
+        self.generator, new_generator = itertools.tee(self.generator)
+        return sum(1 for _ in new_generator)
+        
+
+def organize_output(ner_output, output):
+    print(ner_output)
+    for word in ner_output.ents:
+        # word.ents가 하나의 element만 포함하는지 확인할 필요 있음
+        if len(word.ents) > 1:
+            raise Exception(f"{word.ents}")
+        
+        entity = str(word.ents[0])
+        label = str(word.label_)
+        
+        if not entity in output or label != output[entity]:
+            output[entity] = label
+    return
+
+def organize_output1(entity, label, output):        
+    if not entity in output or label != output[entity]:
+        output[entity] = label
+    return
+
+
+def ner_in_batch_spacy(texts: list, per_document=False) -> dict:
     output = {}
     # To use spaCy in GPU
-    spacy.prefer_gpu()
+    # ValueError("Cannot convert non-numpy from base Ops class") 발생 -> 아직 해결 불가능
+    # flag = spacy.prefer_gpu()
+    # assert flag
     # Faster and smaller pipeline, but less accurate
     # to use "en_core_web_sm", you should download the model first typing "python -m spacy download en_core_web_sm" in terminal
     ner_model = spacy.load("en_core_web_sm", disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"])
     # Larger and slower pipeline, but more accurate
     # to use "en_core_web_trf", you should download the model first typing "python -m spacy download en_core_web_trf" in terminal
     # ner_model = spacy.load("en_core_web_trf", disable=["tagger", "parser", "attribute_ruler", "lemmatizer"])
-    ner_model.max_length = 10000000 
-    ner_outputs = ner_model.pipe(text, n_process=2)
-    for ner_output in ner_outputs:
-        for word in ner_output.ents:
-            # word.ents가 하나의 element만 포함하는지 확인할 필요 있음
-            if len(word.ents) > 1:
-                raise Exception(f"{word.ents}")
+    ner_model.max_length = 10000000
+    # ner_outputs is generator()
+    ner_outputs = ner_model.pipe(texts)
+    
+    if per_document:
+        for i, ner_output in enumerate(ner_outputs):
+            sub_dict = {}
+            for word in ner_output.ents:
+                # word.ents가 하나의 element만 포함하는지 확인할 필요 있음
+                if len(word.ents) > 1:
+                    raise Exception(f"{word.ents}")
+                
+                entity = str(word.ents[0])
+                label = str(word.label_)
+                
+                if not entity in output or label != sub_dict[entity]:
+                    sub_dict[entity] = label
+            output[i] = sub_dict
+        return output
+    else:
+        manager = mp.Manager()
+        output = manager.dict()
+        # Option1
+        start = time.time()
+        for ner_output in ner_outputs:
+            parmap.starmap(organize_output1, [(str(word.ents[0]), str(word.label_)) for word in ner_output.ents], output, pm_processes=100)
+        end = time.time()
+        print(f"Option 1 time : {end-start}")
+        # Option 2
+        start = time.time()
+        parmap.map(organize_output, list(ner_outputs), output, pm_pbar=True, pm_processes=100)
+        end = time.time()
+        print(f"Option 2 time : {end-start}")
+        # Option 3
+        output = {}
+        start = time.time()
+        for ner_output in ner_outputs:
+            for word in ner_output.ents:
+                # word.ents가 하나의 element만 포함하는지 확인할 필요 있음
+                if len(word.ents) > 1:
+                    raise Exception(f"{word.ents}")
+                
+                entity = str(word.ents[0])
+                label = str(word.label_)
+                
+                if not entity in output or label != output[entity]:
+                    output[entity] = label
+        end = time.time()
+        print(f"Option 3 time : {end-start}")
+        
+    # for ner_output in ner_outputs:
+    #     print(len(list(ner_output.ents)), end=" ")
+    #     for word in ner_output.ents:
+    #         # word.ents가 하나의 element만 포함하는지 확인할 필요 있음
+    #         if len(word.ents) > 1:
+    #             raise Exception(f"{word.ents}")
             
-            entity = str(word.ents[0])
-            label = str(word.label_)
+    #         entity = str(word.ents[0])
+    #         label = str(word.label_)
             
-            if not entity in output or label != output[entity]:
-                output[entity] = label
+    #         if not entity in output or label != output[entity]:
+    #             output[entity] = label
             
-            # if str(word.ents[0]) in output.keys() and str(word.label_) == output[str(word.ents[0])]["label"]:
+    #         # if str(word.ents[0]) in output.keys() and str(word.label_) == output[str(word.ents[0])]["label"]:
             #     output[str(word.ents[0])]['count'] += 1
             # else:
             #     output[str(word.ents[0])] = {"label": str(word.label_), "step": [step_idx]}
-    return output
+        # print(output)
+    # return output
 
 
 if __name__=="__main__":
-    t = ["Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time. Best known for developing the theory of relativity, Einstein also made important contributions to quantum mechanics, and was thus a central figure in the revolutionary reshaping of the scientific understanding of nature that modern physics accomplished in the first decades of the twentieth century." for i in range(1000)]
+    t = ["Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time. Best known for developing the theory of relativity, Einstein also made important Albert Einstein Albert Einstein contributions to quantum mechanics, and was thus a central figure in the revolutionary reshaping of the scientific understanding of nature that modern physics accomplished in the first decades of the twentieth century." for i in range(5)]
+    t.append("Hello my name is Jinho")
+    t.append("I'm majoring AI in KAIST")
+    print(t)
     start = time.time()
-    print(ner_in_batch_spacy(t))
+    print(ner_in_batch_spacy(t, per_document=True))
     end = time.time()
     print(end-start)
