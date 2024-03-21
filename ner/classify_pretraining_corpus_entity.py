22bce67823139435d88b267e5216b1e2d0bb61c4
Bread0288 <binlepain178@gmail.com>
2024-03-21 11:58:49 +0000

    Add: option to choose GPU or CPU

--- ner/classify_pretraining_corpus_entity.py
+++ ner/classify_pretraining_corpus_entity.py
@@ -5,9 +5,7 @@ from olmo.data import build_memmap_dataset
 from transformers import AutoTokenizer
 import numpy as np
 import torch
-from tqdm import tqdm
-import time
-import json
+import json, argparse, time
 
 
 data_order_file_path = cached_path("https://olmo-checkpoints.org/ai2-llm/olmo-small/46zc5fly/train_data/global_indices.npy")
@@ -29,18 +27,30 @@ def get_batch_instances(global_indices, dataset, batch_size: int, batch_idx: int
     return batch_instances
 
 
-if __name__=="__main__":
+def main(args):
     total_output = {}
-    start_index = 25501
-    end_index = 25501+1
+    start_index = args.start_idx
+    end_index = args.end_idx+1
     tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-1B", trust_remote_code=True)
+    start = time.time()
     for step_idx in range(start_index, end_index):
         batch = torch.tensor(get_batch_instances(global_indices, dataset, batch_size, batch_idx=step_idx))
         # <class: 'list'>, len : 2048
         batch_in_text = tokenizer.batch_decode(batch, skip_special_tokens=True)
-        ner_result = ner_in_batch_spacy(batch_in_text)
+        ner_result = ner_in_batch_spacy(batch_in_text, is_gpu_version=args.is_gpu_version)
         
         total_output[step_idx] = ner_result
-        with open(f"../results/entity_{start_index}-{step_idx}.json", "w") as f:
-            json.dump(total_output, f)
+    with open(f"../results/entity_{start_index}-{args.end_idx}.json", "w") as f:
+        json.dump(total_output, f)
+    end = time.time()
+    print(f"time : {end-start}")
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("start_idx", type=int, help="the index of first batch to determine the entities contained within.")
+    parser.add_argument("end_idx", type=int, help="the index of last batch to determine the entities contained within.")
+    parser.add_argument("--is_gpu_version", action="store_true", help="In NER, we have two version for using CPU and GPU, you have to choose")
     
+    args = parser.parse_args()
+    main(args)
--- ner/ner.py
+++ ner/ner.py
@@ -34,7 +34,7 @@ def organize_flair_output(sentence, output):
     return
 
 
-def ner_in_batch_spacy(texts: list, per_document=False) -> dict:
+def ner_in_batch_spacy(texts: list, per_document=False, is_gpu_version=False) -> dict:
     output = {}
     # To use spaCy in GPU
     # ValueError("Cannot convert non-numpy from base Ops class") 발생 -> 아직 해결 불가능
@@ -46,11 +46,8 @@ def ner_in_batch_spacy(texts: list, per_document=False) -> dict:
     # Larger and slower pipeline, but more accurate
     # to use "en_core_web_trf", you should download the model first typing "python -m spacy download en_core_web_trf" in terminal
     # ner_model = spacy.load("en_core_web_trf", disable=["tagger", "parser", "attribute_ruler", "lemmatizer"])
-    start = time.time()
     # ner_outputs is generator()
     ner_outputs = ner_model.pipe(texts)
-    end = time.time()
-    print(f"ner_model.pipe() time : {end-start}")
     
     if per_document:
         for i, ner_output in enumerate(ner_outputs):
@@ -70,54 +67,32 @@ def ner_in_batch_spacy(texts: list, per_document=False) -> dict:
     else:
         manager = mp.Manager()
         output = manager.dict()
-        # Option1
-        # start = time.time()
-        # for ner_output in tqdm(ner_outputs):
-        #     parmap.starmap(organize_output1, [(str(word.ents[0]), str(word.label_)) for word in ner_output.ents], output, pm_processes=10)
-        # end = time.time()
-        # print(f"Option 1 time : {end-start}")
-        # Option 2
-        start = time.time()
-        parmap.map(organize_output, list(ner_outputs), output, pm_pbar=True, pm_processes=10)
-        end = time.time()
-        print(f"Option 2 sorting time : {end-start}")
-        # Option 3
-        # output = {}
-        # start = time.time()
-        # for ner_output in ner_outputs:
-        #     for word in ner_output.ents:
-        #         # word.ents가 하나의 element만 포함하는지 확인할 필요 있음
-        #         if len(word.ents) > 1:
-        #             raise Exception(f"{word.ents}")
-                
-        #         entity = str(word.ents[0])
-        #         label = str(word.label_)
-                
-        #         if not entity in output or label != output[entity]:
-        #             output[entity] = label
-        # end = time.time()
-        # print(f"Option 3 time : {end-start}")
-        # Option 4 : Using Flair
-        # flair.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # tagger = SequenceTagger.load("flair/ner-english-fast")
-        # start = time.time()
-        # sentences = [Sentence(text) for text in texts]
-        # end = time.time()
-        # print(f"Make Sentence time : {end-start}")
-        # start = time.time()
-        # # output is still List
-        # tagger.predict(sentences, verbose=True, mini_batch_size=128)
-        # end = time.time()
-        # print(f"tagger.predict() time : {end-start}")
-        # start = time.time()
-        # sentences = [sentence.to_dict() for sentence in sentences]
-        # end = time.time()
-        # print(f"Make Dict time : {end-start}")
-        # start = time.time()
-        # parmap.map(organize_flair_output, sentences, output=output, pm_pbar=True, pm_processes=20)
-        # end = time.time()
-        # print(f"Option 4 sorting time : {end-start}")
-        
+        if is_gpu_version:
+            flair.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            tagger = SequenceTagger.load("flair/ner-english-fast")
+            # start = time.time()
+            sentences = [Sentence(text) for text in texts]
+            # end = time.time()
+            # print(f"Make Sentence time : {end-start}")
+            # start = time.time()
+            # output is still List
+            tagger.predict(sentences, verbose=True, mini_batch_size=128)
+            # end = time.time()
+            # print(f"tagger.predict() time : {end-start}")
+            # start = time.time()
+            sentences = [sentence.to_dict() for sentence in sentences]
+            # end = time.time()
+            # print(f"Make Dict time : {end-start}")
+            # start = time.time()
+            parmap.map(organize_flair_output, sentences, output=output, pm_pbar=True, pm_processes=20)
+            # end = time.time()
+        else:
+            # start = time.time()
+            parmap.map(organize_output, list(ner_outputs), output, pm_pbar=True, pm_processes=10)
+            # end = time.time()
+            # print(f"Option 2 sorting time : {end-start}")
+
+
         return dict(output)
 
 
