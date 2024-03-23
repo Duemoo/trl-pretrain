0ff9b6d28fd9ec0bd0b6e9a4f63b2a17d49f927f
Duemoo <retapurayo@snu.ac.kr>
2024-03-24 02:14:15 +0900

    fix bugs in drawing violin plots and add success check

--- scratch/ppl_analysis.py
+++ scratch/ppl_analysis.py
@@ -16,26 +16,51 @@ import pandas as pd
 import seaborn as sns
 
 
-def draw_violin(sim_dict, ppl, interval, hard, exp_name):
+def fraction_larger_than_first(lst):
+    if len(lst) <= 1:
+        return 0  # Avoid division by zero if the list has one or no elements
+    
+    first_value = lst[0]
+    count_larger = sum(1 for x in lst[1:] if x > first_value)
+    
+    return count_larger / (len(lst) - 1)
+
+
+def check_success(ppls, threshold=0.2, debug=False):
+    if all([ppls[0]>=ppls[i] for i in range(len(ppls))]):
+    # if ppls[0]>ppls[2]:
+        if fraction_larger_than_first(ppls)<threshold:
+            return True
+    else:
+        if debug:
+            if ppls[0]>ppls[1]:
+                print("Fail due to the initial ppl increase")
+            if fraction_larger_than_first(ppls)<threshold:
+                print("Fail due to the threshold condition")
+        return False
+
+
+def draw_violin(sim_dict, ppl, ppl_success, hard, interval, exp_name):
     sim_jaccard = sim_dict["jaccard"]
-    sim_rouge = sim_dict["rouge_l"]
 
-    # Remove outliers based on IQR for ppl
-    Q1, Q3 = np.percentile(ppl, [25, 75])
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 2.0 * IQR
-    upper_bound = Q3 + 2.0 * IQR
-    filtered_indices = [i for i, value in enumerate(ppl) if lower_bound <= value <= upper_bound]
-
-    sim_jaccard_filtered = [sim_jaccard[i] for i in filtered_indices]
-    sim_rouge_filtered = [sim_rouge[i] for i in filtered_indices]
-    ppl_filtered = [ppl[i] for i in filtered_indices]
+    def filter_data(ppl_data, sim):
+        # Remove outliers based on IQR for ppl
+        Q1, Q3 = np.percentile(ppl_data, [25, 75])
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 2.0 * IQR
+        upper_bound = Q3 + 2.0 * IQR
+        filtered_indices = [i for i, value in enumerate(ppl_data) if lower_bound <= value <= upper_bound]
+        sim_filtered = [sim[i] for i in filtered_indices]
+        ppl_filtered = [ppl_data[i] for i in filtered_indices]
+        return sim_filtered, ppl_filtered
+        
+    sim_all_filtered, ppl_all_filtered = filter_data(ppl, sim_jaccard)
+    sim_success_filtered, ppl_success_filtered = filter_data(ppl_success, sim_jaccard)
 
     # Setup the figure with subplots
     fig, axes = plt.subplots(2, 2, figsize=(24, 16), gridspec_kw={'height_ratios': [3, 1]})
-    sim_metrics = [("jaccard", sim_jaccard_filtered), ("rouge_l", sim_rouge_filtered)]
 
-    for i, (label, sim_filtered) in enumerate(sim_metrics):
+    for i, (sim_filtered, ppl_filtered) in enumerate([(sim_all_filtered, ppl_all_filtered), (sim_success_filtered, ppl_success_filtered)]):
         # Binning the data
         bins = np.linspace(min(sim_filtered), max(sim_filtered), num=9)
         bin_labels = [f"{bins[i]:.2f} - {bins[i+1]:.2f}" for i in range(len(bins)-1)]
@@ -48,22 +73,24 @@ def draw_violin(sim_dict, ppl, interval, hard, exp_name):
             'Value': ppl_filtered
         })
 
+        data.sort_values('Group', inplace=True)
+        
         # Violin plot
         sns.violinplot(ax=axes[0, i], x='Group', y='Value', data=data)
-        axes[0, i].set_title(f'Violin plot of {label} with outliers removed')
+        # axes[0, i].set_title(f'Violin plo with outliers removed')
         axes[0, i].set_xlabel('Bins of sim (intervals)')
         axes[0, i].set_ylabel('ppl values')
         axes[0, i].tick_params(axis='x', rotation=45)
 
         # Histogram
         sns.histplot(ax=axes[1, i], x=sim_filtered, bins=bins, kde=False)
-        axes[1, i].set_title(f'Histogram of {label}')
-        axes[1, i].set_xlabel(label)
+        # axes[1, i].set_title(f'Histogram of {label}')
+        # axes[1, i].set_xlabel(label)
         axes[1, i].set_ylabel('Count')
         axes[1, i].set_xticks(bins)
         axes[1, i].tick_params(axis='x', rotation=45)
 
-    plt.tight_layout()
+        plt.tight_layout()
 
     # Create the directory if it doesn't exist
     os.makedirs(f"violin/{exp_name}", exist_ok=True)
@@ -167,8 +194,8 @@ def remove_outliers_iqr(data, multiplier=2, log=False):
 
 def load_json(path):
     with open(path) as f:
-        return [json.loads(l.strip()) for l in f]
-        # return json.load(f)
+        # return [json.loads(l.strip()) for l in f]
+        return json.load(f)
 
 
 def mean(l):
@@ -269,11 +296,15 @@ def measure_scores(result, train_indices, premem=False, interval=10000):
     gen_learnability_per_ex = []
     gen_learnability_easy_per_ex = []
     gen_learnability_hard_per_ex = []
+    gen_success_learnability_easy_per_ex = []
+    gen_success_learnability_hard_per_ex = []
     gen_fluc_per_ex = []
     mem_fluc_per_ex = []
     similarity_easy_per_ex = {"jaccard": [], "rouge_l": []}
     similarity_hard_per_ex = {"jaccard": [], "rouge_l": []}
     freq = None
+    success_count_easy=0
+    success_count_hard=0
 
     for ex_idx in tqdm(range(len(probe_ppls))):
 
@@ -309,10 +340,18 @@ def measure_scores(result, train_indices, premem=False, interval=10000):
                     last_ppl=ppls[sp+interval]
                 gen_freq_per_ex.append(freq_y)
                 gen_learnability_per_ex.append((1-last_ppl/init_ppl))
+
+                values_with_prev = ppls[train_idx[-1]-1:train_idx[-1]+margin]
                 if j<5:
                     gen_learnability_easy_per_ex.append((1-last_ppl/init_ppl))
+                    if check_success(values_with_prev):
+                        success_count_easy += 1
+                        gen_success_learnability_easy_per_ex.append((1-last_ppl/init_ppl))
                 else:
                     gen_learnability_hard_per_ex.append((1-last_ppl/init_ppl))
+                    if check_success(values_with_prev):
+                        success_count_hard += 1
+                        gen_success_learnability_hard_per_ex.append((1-last_ppl/init_ppl))
                 # gen_fluc_per_ex.append(1-last_ppl/min_ppl)
                 # segment = ppls[sp:sp+400]
                 # gen_fluc = calculate_fluc(segment)
@@ -353,7 +392,6 @@ def measure_scores(result, train_indices, premem=False, interval=10000):
                 # Frequency analysis
                 _, freq_y = spectrum_analysis(train_ppl[sp:sp+400])
                 mem_freq_per_ex.append(freq_y)
-                intervals = [100, 500, 1000, 5000, 10000, 15000]
                 if interval>0:
                     last_ppl=train_ppl[round(sp+interval)]
                 else:
@@ -388,8 +426,8 @@ def measure_scores(result, train_indices, premem=False, interval=10000):
 
             mem_fluc_per_ex.append((train_ppl[round(500+interval)]-min_ppl)/abs(train_ppl[500]-min_ppl))
 
-    draw_violin(similarity_easy_per_ex, gen_learnability_easy_per_ex, interval, hard=False, exp_name=args.exp_name[0][:-5])
-    draw_violin(similarity_hard_per_ex, gen_learnability_hard_per_ex, interval, hard=True, exp_name=args.exp_name[0][:-5])
+    draw_violin(similarity_easy_per_ex, gen_learnability_easy_per_ex, gen_success_learnability_easy_per_ex, hard=False, interval=args.interval, exp_name=args.exp_name[0][:-5])
+    draw_violin(similarity_hard_per_ex, gen_learnability_hard_per_ex, gen_success_learnability_hard_per_ex, hard=True, interval=args.interval, exp_name=args.exp_name[0][:-5])
 
     # remove outliers
     if not premem:
@@ -406,6 +444,8 @@ def measure_scores(result, train_indices, premem=False, interval=10000):
             gen_learnability_per_ex = remove_outliers_iqr(gen_learnability_per_ex, log=True)
             gen_learnability_easy_per_ex = remove_outliers_iqr(gen_learnability_easy_per_ex, log=True)
             gen_learnability_hard_per_ex = remove_outliers_iqr(gen_learnability_hard_per_ex, log=True)
+            gen_success_learnability_easy_per_ex = remove_outliers_iqr(gen_success_learnability_easy_per_ex, log=True)
+            gen_success_learnability_hard_per_ex = remove_outliers_iqr(gen_success_learnability_hard_per_ex, log=True)
 
     # Plot averqge frequency spectrum
     mem_freq = mean_of_arrays(mem_freq_per_ex)
@@ -448,9 +488,14 @@ def measure_scores(result, train_indices, premem=False, interval=10000):
         # print(f"gen_learnability_stdev: {statistics.pstdev(gen_learnability_per_ex)}")
         print()
         print(f"gen_learnability_easy: {mean(gen_learnability_easy_per_ex)}")
+        print(f"gen_success_learnability_easy: {mean(gen_success_learnability_easy_per_ex)}")
         # print(f"gen_learnability_easy_stdev: {statistics.pstdev(gen_learnability_per_ex)}")
         print()
         print(f"gen_learnability_hard: {mean(gen_learnability_hard_per_ex)}")
+        print(f"gen_success_learnability_hard: {mean(gen_success_learnability_hard_per_ex)}")
+        print()
+        print(f"easy success fraction: {success_count_easy/len(gen_learnability_easy_per_ex)}")
+        print(f"hard success fraction: {success_count_hard/len(gen_learnability_hard_per_ex)}")
         # print(f"gen_learnability_hard_stdev: {statistics.pstdev(gen_learnability_per_ex)}")
         # print(f"gen_fluc: {mean(gen_fluc_per_ex)}")
         # print(f"len_notrain: {len(pre_mem_fluc_per_ex)}")
@@ -486,26 +531,32 @@ def plot_ppl_with_trained_at(results, exp_names=['105b', '1T', '2T', '3T'], save
 
     # plt.figure(figsize=(16, 20))
     for ex_idx in tqdm(range(len(all_probe_ppls[0]))):
-        # plt.figure(figsize=(32, 15))
-        plt.figure(figsize=(16, 30))
+        plt.figure(figsize=(32, 15))
+        # plt.figure(figsize=(16, 30))
 
         for result_idx in range(len(results)):
             n_probes = len(all_probe_ppls[result_idx][ex_idx][0])
             # print(n_probes)
             for j in range(n_probes):
-                # if j!=n_probes-1:
-                    # continue
+                margin=50
+                if len(train_indices[ex_idx])>0:
+                    last_train_idx = train_indices[ex_idx][-1]
+                    values=[all_probe_ppls[result_idx][ex_idx][i-2][j] for i in range(last_train_idx, last_train_idx+50)]
+                    # print(values)
+                    color = 'blue' if check_success(values) else 'gray'
+                else:
+                    color = 'green'
+                
                 plt.subplot(1+n_probes, len(results), result_idx+1+j*len(results))
-                # print(f"steps: {len(steps)}\n\nppls: {len()}")
-                plt.plot(steps, [d[j] for d in all_probe_ppls[result_idx][ex_idx]])
+                plt.plot(steps, [d[j] for d in all_probe_ppls[result_idx][ex_idx]], color=color)
                 try:
                     x_vals=[i-101 for i in train_indices[ex_idx]]
                     y_vals=[all_probe_ppls[result_idx][ex_idx][i-1][j] for i in train_indices[ex_idx]]
-                    plt.scatter(x_vals, y_vals, color='red', s=12)
+                    plt.scatter(x_vals, y_vals, color='red', s=24)
 
                     x_prev_vals=[i-102 for i in train_indices[ex_idx]]
                     y_prev_vals=[all_probe_ppls[result_idx][ex_idx][i-2][j] for i in train_indices[ex_idx]]
-                    plt.scatter(x_prev_vals, y_prev_vals, color='black', s=12)
+                    plt.scatter(x_prev_vals, y_prev_vals, color='black', s=24)
                 except:
                     pass
 
@@ -622,7 +673,7 @@ if __name__ == '__main__':
 
 
     # Add arguments
-    parser.add_argument('--base_dir', type=str, default="/mnt/nas/hoyeon/trl-pretrain/")
+    parser.add_argument('--base_dir', type=str, default="/data/hoyeon/trl-pretrain/")
     parser.add_argument('--save_dir', type=str, default="test")
     parser.add_argument('--exp_name', nargs='+', required=True)
     parser.add_argument('--text_log', type=str, required=True)
